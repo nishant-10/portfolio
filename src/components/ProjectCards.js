dd9b943fe76625f7b128b8ab8af41d28aa7bb15b
Nishant Joshi <nishantjoshi863@gmail.com>
2022-05-12 01:20:50 +0530

    fixed card css and added show more button

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import "../css/projectcards.css";
 import Cards from "./Cards";
 const data = require("../data/ProjectsCard.json");
 function ProjectCards() {
+  const [visible, setVisible] = useState(2);
+  const [allCardsShown, setallCardsShown] = useState(false);
+  const morecards = () => {
+    let element = document.getElementById("showMoreButt");
+    // var t = ReactDOM.findDOMNode(element);
+    // element.innerText = "SHOW LESS";
+
+    if (visible < data.projects.length) {
+      element.innerText = "SHOW MORE";
+      setVisible((prevValue) => prevValue + 2);
+    }
+    if (visible >= data.projects.length - 2) {
+      if (allCardsShown) {
+        element.innerText = "SHOW MORE";
+        setallCardsShown(false);
+        setVisible(2);
+      }
+      if (!allCardsShown) {
+        setallCardsShown(true);
+        element.innerText = "SHOW LESS";
+        setVisible((prevValue) => prevValue + 2);
+      }
+    }
+  };
   return (
     <>
       <div className="cardArea">
         <h1 className="cah1 cardAreaTitle">Noteworthy Projects</h1>
         <h1 className="cah1 cardAreaSubTitle">Made for colleges</h1>
         <div className="cards">
-          <Cards />
+          <Cards visiblecards={visible} />
         </div>
+        <button id="showMoreButt" onClick={morecards}>
+          SHOW MORE
+        </button>
       </div>
     </>
   );
